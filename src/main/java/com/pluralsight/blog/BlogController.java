6d93f376cab8273b3b55d24fbe0be71040f8969e
Stefan Nychka <stefannychka@gmail.com>
2019-12-09 20:28:37 -0700

    module 2 task 6 error

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,25 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository p) {
+        postRepository = p;
+    }
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
-        modelMap.put("title", "Blog Post 1");
+        List<Post> p = postRepository.getAllPosts();
+        modelMap.put("post", p);
         return "home";
     }
 }
